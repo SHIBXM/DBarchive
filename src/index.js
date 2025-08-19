e9c938874d34345569481ba4dd6b5dea21aec141
SHIBAM DEY <shibxmx@gmail.com>
2025-08-19 11:29:39 +0530

    Add cookie-parser and cors middleware; update package dependencies

--- src/index.js
+++ src/index.js
@@ -5,16 +5,17 @@ dotenv.config({
 import mongoose from 'mongoose';
 import { DB_NAME } from './constants.js';
 import connectDB from './db/dbIndex.js';
+import app from './app.js';
 
 
 connectDB()
 .then(() => {
-  application.listen(process.env.PORT||8000, () => {
+  app.listen(process.env.PORT||8000, () => {
     console.log(`server is running at port : ${process.env.PORT}`);
   })
 })
 .catch((error) => {
-    console.log("MONGO db connnection failed !!!", err);
+    console.log("MONGO db connnection failed !!!", error);
   
 })
 
